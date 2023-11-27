604db7e3c0fac9694b6e42545501368513305f2b
Yareek95 <yori95ovsii@gmail.com>
2023-11-27 13:55:57 -0600

    bring back <p> tags for weather

--- app.py
+++ app.py
@@ -72,7 +72,14 @@ def index():
     return render_template('index.html', user_input=user_input_weather, weather_data=first_api,
                            second_api_data=second_api_data, currency_data=currency_data, error_message=error_message)
 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+'''    
 if __name__ == '__main__':
     # Use the environment variable PORT if available, or default to 5000
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+'''
