9dd176bc7bd16fa14b9935646bbf45e815a264d8
Yareek95 <yori95ovsii@gmail.com>
2023-11-26 19:07:41 -0600

    Update test_API.py

--- APITests/test_API.py
+++ APITests/test_API.py
@@ -3,7 +3,7 @@ import requests
 
 class TestWeatherAPI(unittest.TestCase):
     BASE_URL = "https://api.weatherapi.com/v1/current.json"
-    API_KEY = "489b3e24494946a89ca63057231911"  # Replace with your API key
+    API_KEY = "489b3e24494946a89ca63057231911"
 
     def test_successful_request(self):
         params = {
@@ -16,6 +16,19 @@ class TestWeatherAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue('current' in response.json())
 
+    def test_town_displayed(self):
+        params = {
+            'key': self.API_KEY,
+            'q': 'London',
+            'lang': 'en',
+        }
+        response = requests.get(self.BASE_URL, params=params)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('current' in response.json())
+        self.assertTrue('location' in response.json())
+        self.assertTrue('name' in response.json()['location'])
+
     def test_invalid_location(self):
         params = {
             'key': self.API_KEY,
@@ -24,7 +37,7 @@ class TestWeatherAPI(unittest.TestCase):
         }
         response = requests.get(self.BASE_URL, params=params)
 
-        self.assertEqual(response.status_code, 400)  # Assuming a 400 status code for an invalid location
+        self.assertEqual(response.status_code, 400)
         self.assertTrue('error' in response.json())
 
     def test_missing_api_key(self):
@@ -34,8 +47,25 @@ class TestWeatherAPI(unittest.TestCase):
         }
         response = requests.get(self.BASE_URL, params=params)
 
-        self.assertEqual(response.status_code, 401)  # Assuming a 401 status code for a missing API key
+        self.assertEqual(response.status_code, 401)
         self.assertTrue('error' in response.json())
+
+    def test_response_data(self):
+        params = {
+            'key': self.API_KEY,
+            'q': 'Chicago',
+            'lang': 'en',
+        }
+        response = requests.get(self.BASE_URL, params=params)
+        data = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('location' in data)
+        self.assertTrue('current' in data)
+        self.assertTrue('last_updated' in data['current'])
+        self.assertTrue('temp_c' in data['current'])
+        self.assertTrue('temp_f' in data['current'])
+        # Add more assertions for other response data fields
 
 if __name__ == '__main__':
     unittest.main()
