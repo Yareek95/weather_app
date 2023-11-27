40d04014e3a4009fbc8bd857d97d355841f40470
Yareek95 <yori95ovsii@gmail.com>
2023-11-27 12:33:33 -0600

    add currency

--- app.py
+++ app.py
@@ -4,36 +4,41 @@ import requests
 
 app = Flask(__name__)
 
-BASE_URL = "https://api.weatherapi.com/v1/current.json"
-SECOND_API_BASE_URL = "https://www.amdoren.com/api/weather.php"
-API_KEY = "489b3e24494946a89ca63057231911"
+FIRST_API_BASE_URL = "https://api.weatherapi.com/v1/current.json"   #base weather
+SECOND_API_BASE_URL = "https://www.amdoren.com/api/weather.php"     #forecast weather
+CURRENCY_API_BASE_URL = "https://www.amdoren.com/api/currency.php"  #currency
+
+FIRST_API_KEY = "489b3e24494946a89ca63057231911"
+# SECOND and CURRENCY have the same KEY:
 SECOND_API_KEY = "RSHPS94EemhCEY7QNBfQundpqnUtVk"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    user_input = None
-    weather_data = None
+    user_input_weather = None
+    first_api = None
     second_api_data = None
+    currency_data = None
     error_message = None
 
     if request.method == 'POST':
-        user_input = request.form.get('user_input')
-        if user_input:
-            # First API Call
+        user_input_weather = request.form.get('user_input')
+        user_input_currency_1 = request.form.get('user_input_currency_1')
+        user_input_currency_2 = request.form.get('user_input_currency_2')
+
+        if user_input_weather:
+            # Weather API Call
             params_first_api = {
-                'key': API_KEY,
-                'q': user_input,
+                'key': FIRST_API_KEY,
+                'q': user_input_weather,
                 'lang': 'en',
             }
-            response_first_api = requests.get(BASE_URL, params=params_first_api)
+            response_first_api = requests.get(FIRST_API_BASE_URL, params=params_first_api)
 
             if response_first_api.status_code == 200:
-                weather_data = response_first_api.json()
-
+                first_api = response_first_api.json()
                 # Extract latitude and longitude
-                lat = weather_data['location']['lat']
-                lon = weather_data['location']['lon']
-
+                lat = first_api['location']['lat']
+                lon = first_api['location']['lon']
                 # Second API Call
                 params_second_api = {
                     'api_key': SECOND_API_KEY,
@@ -41,7 +46,6 @@ def index():
                     'lon': lon,
                 }
                 response_second_api = requests.get(SECOND_API_BASE_URL, params=params_second_api)
-
                 if response_second_api.status_code == 200:
                     second_api_data = response_second_api.json()
                 else:
@@ -49,7 +53,23 @@ def index():
             else:
                 error_message = f"Error: {response_first_api.status_code}"
 
-    return render_template('index.html', user_input=user_input, weather_data=weather_data, second_api_data=second_api_data, error_message=error_message)
+        elif user_input_currency_1 and user_input_currency_2:
+            # Currency API Call
+            params_currency_api = {
+                'api_key': SECOND_API_KEY,
+                'from': user_input_currency_1,
+                'to': user_input_currency_2,
+                'amount': 1,  # default amount is 1
+            }
+            response_currency_api = requests.get(CURRENCY_API_BASE_URL, params=params_currency_api)
+
+            if response_currency_api.status_code == 200:
+                currency_data = response_currency_api.json()
+            else:
+                error_message = f"Error: {response_currency_api.json().get('error_message', 'Unknown error')}"
+
+    return render_template('index.html', user_input=user_input_weather, weather_data=first_api,
+                           second_api_data=second_api_data, currency_data=currency_data, error_message=error_message)
 
 if __name__ == '__main__':
     # Use the environment variable PORT if available, or default to 5000
