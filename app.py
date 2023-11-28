0a5c043ef4221afec8fd44cb86890724761f7b6f
Yareek95 <yori95ovsii@gmail.com>
2023-11-27 21:23:10 -0600

    zoom out issue

--- app.py
+++ app.py
@@ -91,10 +91,13 @@ def currency():
 
 '''
 if __name__ == '__main__':
-    app.run(debug=True)
-
-  '''
-if __name__ == '__main__':
     # Use the environment variable PORT if available, or default to 5000
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+    ***test***
+
+  '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
