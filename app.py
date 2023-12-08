c0f5efad48f6a353fa4b130346f4c557c2d54af8
Yareek95 <yori95ovsii@gmail.com>
2023-12-07 20:06:00 -0600

    some

--- app.py
+++ app.py
@@ -172,7 +172,6 @@ def about():
 if __name__ == '__main__':
     app.run(debug=True)
   '''
-
 if __name__ == '__main__':
     # Use the environment variable PORT if available, or default to 5000
     port = int(os.environ.get("PORT", 5000))
