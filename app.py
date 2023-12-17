b21ec06b4bd6d15305b9c99fbc187a51081f6f71
Yareek95 <yori95ovsii@gmail.com>
2023-12-16 18:22:16 -0600

    added socketIO and deleted most styles

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask_bcrypt import Bcrypt
 import requests
 import secrets
 from dotenv import load_dotenv
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
+import random
+from string import ascii_uppercase
 
 load_dotenv()  # Load environment variables from .env file
 app = Flask(__name__)
@@ -20,12 +23,112 @@ SECOND_API_KEY = os.environ.get("SECOND_API_KEY")
 MONGO_DB_PASSWORD = os.environ.get("MONGO_DB_PASSWORD")
 
 app.config['MONGO_URI'] = f"mongodb+srv://Yarik:{MONGO_DB_PASSWORD}@login.wrtuhbw.mongodb.net/weather-app"
+socketio = SocketIO(app)
 mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
+
+rooms = {}
+
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+        if code not in rooms:
+            break
+    return code
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route("/chat", methods=["POST", "GET"])
+def chat():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("chat.html", error="Please enter a name", code=code, name=name)
+
+        if join != False and not code:
+            return render_template("chat.html", error="Please enter a room code", code=code, name=name)
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("chat.html", error="Room does not exist", code=code, name=name)
+
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
+
+    return render_template("chat.html")
+
+
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("chat"))
+
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+
+    # Send the message to the room
+    send(content, to=room)
+
+    # Append the message to the room's messages
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
 
 
 @app.route('/weather', methods=['GET', 'POST'])
@@ -164,14 +267,27 @@ def about():
     username = session.get('username')
     return render_template('about.html', username=username)
 
+@socketio.on('message')
+def handle_message(data):
+    username = session.get('username', 'Guest')
+    message = {'username': username, 'message': data['message']}
+    mongo.db.chat_messages.insert_one(message)
+    emit('message', message, broadcast=True)
+
+
 
 '''
-
-    *** test ***
-if __name__ == '__main__':
-    app.run(debug=True)
-  '''
 if __name__ == '__main__':
     # Use the environment variable PORT if available, or default to 5000
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+    
+    *** test ***
+
+    if __name__ == '__main__':
+    app.run(debug=True)
+    *** test ***
+
+  '''
+if __name__ == '__main__':
+    socketio.run(app, debug=True, use_reloader=False)
